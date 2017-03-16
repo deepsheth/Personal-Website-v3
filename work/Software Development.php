d44a7352294e5a84546f121af1f76fab56d48852
Deep Sheth <contact@deepsheth.com>
2017-03-16 19:38:23 -0400

    been a while since a commit, many changes

--- work/Software Development.php
+++ work/Software Development.php
@@ -15,7 +15,16 @@
 
     <!-- Analytics
 	================================================== -->
+    <script>
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+                                })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
+        ga('create', 'UA-56753882-3', 'auto');
+        ga('send', 'pageview');
+    </script>
+    
     <!-- CSS
 	================================================== -->
     <link href='http://fonts.googleapis.com/css?family=Rajdhani:700|Roboto+Slab:400,700|Asap:400,400italic,700,700italic' rel='stylesheet' type='text/css'>
@@ -44,86 +53,34 @@
 
             <div class="container">
                 <h2>Compilers & Assemblers</h2>
-                <p>
+                
+                <div class="row">
+                <p class="col-md-6">
                     Last semester at Lehigh, I took a Systems Software course <a href="">(CSE 109)</a>. One of the main goals in this course is to explore how computers "understand" programming languages. I developed a compiler for a simplified high-level language. The compiler tokenizes and parses the pseudo language. Once the parser successfully runs, the compiler outputs SASM assembly code in a new file. On top of this, I developed an assembler which takes the SASM assembly code and converts it to machine code (hexadecimal values were used instead of binary values).
                 </p>
-
-                <div class="row content-footer">
-                    <div class="col-sm-3">
-                        <div class="card">
-                            <a href="#" target="_blank">
-                                <div class="card-footer">
-                                    Compiler
-                                </div>
-                            </a>
-                        </div>
-                    </div>
-                    <div class="col-sm-3">
-                        <div class="card">
-                            <a href="#" target="_blank">
-                                <div class="card-footer">
-                                    Assembler
-                                </div>
-                            </a>
-                        </div>
-                    </div>
+                <img src="/work/pic/Parser.PNG" alt="Parsing a while statement" class="col-md-6">
                 </div>
 
                 <h2>Trie</h2>
                 <p>
-                    Tries are a useful data structure for storing dictionray terms, since it is very efficient to look up/retrieve words. Each node in my Trie data structure holds up to 10 links. Links hold individual letters, and nodes have a property which indicates if it's the end of a word. This design allows for compound words to be stored and retrieved. For example, if <strong>POPCORN</strong> is stored into the trie, and then <strong>POP</strong> is inserted, new links are <em>not</em> created when <strong>POP</strong> is inserted. However, since nodes indicate that the last "P" in <strong>POP</strong> is the end of a word, <strong>POP</strong> can be retrieved. In addition to holding links, nodes (specifically end nodes) hold an integer value that needs to be passed when inserting a word.
+                    Tries are a useful data structure for storing dictionary terms, since it is very efficient to look up/retrieve words. Each node in my Trie data structure holds up to 10 links. Links hold individual letters, and nodes have a property which indicates if it's the end of a word. This design allows for compound words to be stored and retrieved. For example, if <strong>POPCORN</strong> is stored into the trie, and then <strong>POP</strong> is inserted, new links are <em>not</em> created when <strong>POP</strong> is inserted. However, since nodes indicate that the last "P" in <strong>POP</strong> is the end of a word, <strong>POP</strong> can be retrieved. In addition to holding links, nodes (specifically end nodes) hold an integer value that needs to be passed when inserting a word.
                 </p>
-
-                <div class="row content-footer">
-                    <div class="col-sm-5">
-                        <div class="card">
-                            <a href="#" target="_blank">
-                                <div class="card-footer">
-                                    Trie Data Structure
-                                </div>
-                            </a>
-                        </div>
-                    </div>
-                </div>
 
                 <h2>Spell Checker</h2>
 
                 <p>
-                    Spell checkers that offer suggestions to misspelled words should take the pronouncation of letters into account. This is important because words that are spelled differently may still have the same pronounciation, causing confusion which leads to typos. By using the Soundex algorithm to map letters phonetically, my Spell Checker program provides phonetic based suggestions. In addition, like you would expect, the Spell Checker suggests words very similar to the misspelled word. 
+                    Spell checkers that offer suggestions to misspelled words should take the pronunciation of letters into account. This is important because words that are spelled differently may still have the same pronunciation, causing confusion which leads to typos. By using the Soundex algorithm to map letters phonetically, my Spell Checker program provides phonetic based suggestions. In addition, like you would expect, the Spell Checker suggests words very similar to the misspelled word. 
                 </p>
                 
                 <p>
-                    Before provding any suggestions to a typo, my program ranks the phonetically similar and similar spelled words based on my algorithm. The program narrows down to about 20 similar words and sorts the suggestions based on an algorithm that gives each word a confidence level. The top five words with the highest confidence rank are outputted as suggestions to the user. Fewer (or none) words may be suggested to the user if the confidence level of a word does not meet the minimum threshold. Therefore, for completely random words like "zyuilp", the Spell Checker will not provide any suggestions.
+                    Before providing any suggestions to a typo, my program ranks the phonetically similar and similar spelled words based on my algorithm. The program narrows down to about 20 similar words and sorts the suggestions based on an algorithm that gives each word a confidence level. The top five words with the highest confidence rank are outputted as suggestions to the user. Fewer (or none) words may be suggested to the user if the confidence level of a word does not meet the minimum threshold. Therefore, for completely random words like "zyuilp", the Spell Checker will not provide any suggestions.
                 </p>
-
-                <div class="row content-footer">
-                    <div class="col-sm-5">
-                        <div class="card">
-                            <a href="#" target="_blank">
-                                <div class="card-footer">
-                                    Download Spell Checker
-                                </div>
-                            </a>
-                        </div>
-                    </div>
-                </div>
 
                 <h2>Poker Hand Ranker</h2>
                 <p>
                     Players who are new to poker may need help understanding the different types of hands in poker, and which hand in poker is ranked higher. This program makes it easy for a player to input their hand of 5 cards, and then the program outputs the rank of their cards (ex. Royal Flush).
                 </p>
 
-                <div class="row content-footer">
-                    <div class="col-sm-6">
-                        <div class="card">
-                            <a href="#" target="_blank">
-                                <div class="card-footer">
-                                    Download Poker Hand Ranker
-                                </div>
-                            </a>
-                        </div>
-                    </div>
-                </div>
 
             </div>
         </section>
--- work/Web Development.php
+++ work/Web Development.php
@@ -15,6 +15,16 @@
 
     <!-- Analytics
 	================================================== -->
+    <script>
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+                                })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+        ga('create', 'UA-56753882-3', 'auto');
+        ga('send', 'pageview');
+    </script>
+    
 
     <!-- CSS
 	================================================== -->
@@ -31,56 +41,125 @@
 <body>
     <?php include '../partials/header.php'; ?>
 
-   <img class="full-width" src="./pic/Web_Design.jpg">
-   
-    <section id="main-content">
-        <div class="post-title">
-            <h1>Web Development</h1>
-            <ul class="post-info">
-                <li>Deep Sheth</li>
-                <li>Jan. 1, 2016</li>
-            </ul>
-        </div>
-        
-        <div class="container">
-            <h2>Website Redesigns</h2>
-            <p>
+        <img class="full-width" src="./pic/Web_Design.jpg">
+
+        <section id="main-content">
+            <div class="post-title">
+                <h1>Web Development</h1>
+                <ul class="post-info">
+                    <li>Deep Sheth</li>
+                    <li>Jan. 1, 2016</li>
+                </ul>
+            </div>
+
+            <div class="container">
+                <h2>Website Redesigns</h2>
+                <p>
+                    Everytime I design a website, I try to explore a new aspect of webdesign I have not used much before. For example, the first time I released my personal website, I used Twitter's popular Bootstrap framework for my website's layout. A year later, I redesigned my website using <a href="https://github.com/shetharp/CobbleStone" target="_blank">CobbleStone</a>, a simple front-end framework I created. I opted for a more colorful and more interactive website. After designing the website, I converted it into a WordPress theme, allowing me to blog. Now, I'm at Version 3 where I have tried to emphasize minimalism. This time, I use SCSS for the design and have moved away from front-end frameworks. This is my third iteration of my personal website. I always try to learn something new each time I design another website. The first time I released my personal website, I worked with the popular framework Bootstrap
+                </p>
+
+                <h2>Hawk Yak</h2>
+                <p>
+                    When I came into Lehigh as a freshmen, Yik Yak was a very popular app used by many college students. However, the app was exclusive to phones and there was no website that mirrored local yaks onto the web. With the help of my roommate, we used an API to expand yakking capabilities to the browser. I designed the website which dynamically displayed local yaks. With the help of web.py, my partner transferred the local yak feed data from python into javasript, which I then handled.
+                </p>
+                <p>We also enabled voting on yaks (without having an account) so to prevent exessive vote manipulation we kept the website exclusive to only students connected to the Lehigh network</p>
+                <p>Now, Yik Yak has finally released their own web app. Looking back, our vision of Yik Yak's web app is similar to what has been released. Hawk Yak allowed users to click yaks to expand to the comments. We also enabled effortless switching between new and hot yaks. As a startup, Yik Yak has been a fast growing company which introduced many new featured after we developed Hawk Yak. The API we used no longer works, so we discontinued the project even though we had many creative ideas planned for making Yik Yak more engaging and vibrant.</p>
+
+                <h2>Front-end Framework: CobbleStone</h2>
+                <p>
+                    CobbleStone is a front-end framework that I helped develop in order to jump start web development projects. CobbleStone enables websites to be easily coded, with its robust grid system and built-in responsive web design features. By making the front-end easy to work with, this allows web developers to focus more on the aesthetics and back-end of a website rather than to worry about why pixels do not precisely align on a webpage. It is extremely easy to implement CobbleStone to any website — the built-in features are heavily documented in order to prevent confusion and make web development more exciting!
+                </p>
+
+                <div class="row content-footer">
+                    <div class="col-sm-5">
+                        <div class="card">
+                            <a href="https://github.com/shetharp/CobbleStone" target="_blank">
+                                <div class="card-footer">
+                                    Download CobbleStone
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                </div>
                 
-            </p>
-            
-            <h2>Hawk Yak</h2>
-            <p>
+
+                <h2 id="coding-the-future">Coding the Future</h2>
+                <p>
+                    I participated in Lehigh's first hackathon ever, sponsored by rVibe. It was 8-hours long, after which the judges voted. The judges agreed that our team had the best execution and a strong vision. We ranked first place and won a nice Samsung Galaxy Tablet from the sponsor!
+                </p>
                 
-            </p>
-            
-            <h2>Front-end Framework: CobbleStone</h2>
-            <p>
-                CobbleStone is a front-end framework that I helped develop in order to jump start web development projects. CobbleStone enables websites to be easily coded, with its robust grid system and built-in responsive web design features. By making the front-end easy to work with, this allows web developers to focus more on the aesthetics and back-end of a website rather than to worry about why pixels do not precisely align on a webpage. It is extremely easy to implement CobbleStone to any website — the built-in features are heavily documented in order to prevent confusion and make web development more exciting!
-            </p>
-            
-            <h2>Project Nudge</h2>
-            <p>
+                <div class="row content-footer">
+                    <div class="col-sm-5">
+                        <div class="card">
+                            <a href="http://thebrownandwhite.com/2015/04/27/coding-house-hackathon/" target="_blank">
+                                <div class="card-footer">
+                                    Brown & White Article
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="col-sm-4">
+                        <div class="card">
+                            <a href="http://johnson-mccormick.com/2015/04/lehigh-hackathon/" target="_blank">
+                                <div class="card-footer">
+                                    Host's Blog Post
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                </div>
                 
-            </p>
-            
-            <h2>Rock, Paper, Scissors</h2>
-            <p>
                 
-            </p>
-            
-            <h2>Fedora Productions</h2>
-            <p>
-                My high school had a student run record label which typically released new albums annually. In order to reach a larger audience, the albums were released on iTunes, CD Baby, and other online platforms. As a member of Fedora Productions, I tried to help the organization grow by designing a website that would serve as the central medium for releasing news, recruiting artists, and selling albums.
-            </p>
-            
-        </div>
-    </section>
+            </div>
+            <div class="row awards">
+                <div class="left-bar col-sm-2 col-md-4">
+                    <div class="">
+                        <img src="../img/award.svg" alt="Achievements Icon">
+                    </div>
+                </div>
+
+                <div class="right-bar col-sm-10 col-md-8">
+                    <div class="container">
+                        <h2>Award Received</h2>
+                        <div class="container">
+                            <p>
+                                1<sup>st</sup> Place Winner
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="container">
+                <h2>Rock, Paper, Scissors</h2>
+                <p>
+                    With my strong interest in design, I took the simple game of rock, paper, scissors, and created a beautiful website for it. The clean design makes the game more appealing.
+                </p>
+                
+                <div class="row content-footer">
+                    <div class="col-sm-5">
+                        <div class="card">
+                            <a href="http://www1.lehigh.edu/news/app-inventors-display-practicality-creativity" target="_blank">
+                                <div class="card-footer">
+                                    Play Game
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+
+                <h2>Fedora Productions</h2>
+                <p>
+                    My high school had a student run record label which typically released new albums annually. In order to reach a larger audience, the albums were released on iTunes, CD Baby, and other online platforms. As a member of this organization, Fedora Productions, I developed a website that would serve as the central medium for releasing news, recruiting artists, and selling albums.
+                </p>
+
+            </div>
+        </section>
 
 
 
-    
 
-<?php include '../partials/footer.php'; ?>
+
+        <?php include '../partials/footer.php'; ?>
 </body>
 
 </html>
